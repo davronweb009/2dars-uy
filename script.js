0ec664c4b82db1add67fcf0edaabc06b2ce7f9f6
davronweb009 <davronweb07@mail.ru>
2025-02-26 16:51:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,613 @@
+document.getElementById("price").onclick = function(){
+    let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+    switch(+soroq){
+        case 1:
+          
+        let pro8 = confirm(`Bitta Set 45 000 so'm boladai
+        Buyurtma berasizmi
+          `)
+          alert('Buyurtma qabul qilindi')
+        break;
+        case 2:
+        let pro1 = confirm(`Ikkita Set 90 000 so'm boladai
+        Buyurtma berasizmi
+          `)
+          alert('Buyurtma qabul qilindi')
+        break;
+        case 3:
+        let pro2 = confirm(`Uchta Set 135 000 so'm boladai
+        Buyurtma berasizmi
+          `)
+          alert('Buyurtma qabul qilindi')
+        break;
+        case 4:
+        let pro3 = confirm(`Tortta Set 180 000 so'm boladai
+        Buyurtma berasizmi
+          `)
+          alert('Buyurtma qabul qilindi')
+        break;
+        case 5:
+        let pro4 = confirm(`Beshta Set 225 000 so'm boladai
+        Buyurtma berasizmi
+          `)
+          alert('Buyurtma qabul qilindi')
+        break;
+        case 6:
+        let pro5 = confirm(`Oltita Set 270 000 so'm boladai
+        Buyurtma berasizmi
+          `)
+          alert('Buyurtma qabul qilindi')
+        break;
+        case 7:
+        let pro6 = confirm(`Yettit Set 315 000 so'm boladai
+        Buyurtma berasizmi
+          `)
+          alert('Buyurtma qabul qilindi')
+        break;
+        default:
+          let pro = confirm("Buncha set tayorlashning iloji yoq")
+    }
+}
+
+document.getElementById("price2").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 79 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  79000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  79000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  79000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  79000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  79000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  79000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+document.getElementById("price3").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 34 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  34000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  34000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  34000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  34000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  34000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  34000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+
+document.getElementById("price4").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 45 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  45000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  45000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  45000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  45000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  45000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  45000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+
+document.getElementById("price5").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 62 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  62000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  62000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  62000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  62000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  62000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  62000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+document.getElementById("price6").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 53 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  53000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  53000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  53000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  53000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  53000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  53000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+document.getElementById("price7").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 36 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  36000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  36000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  36000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  36000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  36000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  36000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+document.getElementById("price8").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 20 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  20000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  20000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  20000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  20000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  20000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  20000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+document.getElementById("price9").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 43 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  43000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  43000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  43000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  43000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  43000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  43000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+document.getElementById("price10").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 79 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  79000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  79000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  79000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  79000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  79000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  79000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+document.getElementById("price11").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 149 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+  149000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+  149000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+  149000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+  149000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+  149000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+  149000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
+
+document.getElementById("price12").onclick = function(){
+  let soroq = prompt("Nechta buyurtma qilmoqchisiz")
+  switch(+soroq){
+      case 1:
+        
+      let pro8 = confirm(`Bitta Set 43 000 so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 2:
+      let pro1 = confirm(`Ikkita set `+ 43000 * 2 +` So'm bo'ladi
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 3:
+      let pro2 = confirm(`Uchta set `+ 43000 * 3 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 4:
+      let pro3 = confirm(`Tortta set `+ 43000 * 4 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 5:
+      let pro4 = confirm(`Beshta set `+ 43000 * 5 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 6:
+      let pro5 = confirm(`Oltita set `+ 43000 * 6 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      case 7:
+      let pro6 = confirm(`Yettita set `+ 43000 * 7 +` so'm boladai
+      Buyurtma berasizmi
+        `)
+        alert('Buyurtma qabul qilindi')
+      break;
+      default:
+        let pro = confirm("Buncha set tayorlashning iloji yoq")
+  }
+}
